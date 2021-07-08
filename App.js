d763a943490c3a2003f9b3e5ff3da569557cd3f7
Diego Torres <dtorres@binwus.com>
2021-07-08 17:09:02 -0400

    commit de prueba

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Diego</Text>
+      <Text>Hola Diego y Leo</Text>
       <StatusBar style="auto" />
     </View>
   );
