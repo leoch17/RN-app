b63e0c3eae033da86d9591c65b2cc1e1c374e6bf
Leonardo Chourio <leoch1000@gmail.com>
2021-07-13 21:59:33 -0400

    ListScreen listo

--- components/Lists.js
+++ components/Lists.js
@@ -1,39 +1,55 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { useSelector } from "react-redux";
 //import {fonts} from '../styles/CustomFonts';
 
-import globalStyles from '../styles/global';
+import globalStyles from "../styles/global";
 
-const Lists = () => {
-    const { lists } = useSelector(state => state.list);
-    console.log(lists); 
+const Lists = ({ navigation }) => {
+  const { lists } = useSelector((state) => state.list);
+  const itemClickHandler = (item) => {
+    navigation.navigate("List", { name: item.name, id: item.id });
+  };
 
-    return(
-        <View style={styles.container}> 
-            {lists.length > 0 ? <FlatList
-                keyExtractor={(item) => item.id}
-                contentContainerStyle={globalStyles.listContainer}
-                data={lists}
-                renderItem={({ item }) => <TouchableOpacity style={globalStyles.listItem} onPress={() => itemClickHandler(item)} >
-                    <Text style={styles.itemText}>{item.name}</Text>
-                </TouchableOpacity>}
-            /> : <Text style={globalStyles.noData}>Sin listas</Text>}
-        </View>    
-        );
+  return (
+    <View style={styles.container}>
+      {lists.length > 0 ? (
+        <FlatList
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={globalStyles.listContainer}
+          data={lists}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={globalStyles.listItem}
+              onPress={() => itemClickHandler(item)}
+            >
+              <Text style={styles.itemText}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      ) : (
+        <Text style={globalStyles.noData}>Sin listas</Text>
+      )}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        paddingVertical: 20,
-        flex: 1,
-    },
-    itemText: {
-    fontFamily: 'Ubuntu-Regular',
-    fontSize: 16,
-    color: '#000',
+  container: {
+    paddingVertical: 20,
+    flex: 1,
   },
-     
-}); 
+  itemText: {
+    fontFamily: "Ubuntu-Regular",
+    fontSize: 16,
+    color: "#000",
+  },
+});
 
 export default Lists;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,39 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
-import { Colors } from './../constants/index';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { Colors } from "./../constants/index";
+import { useDispatch } from "react-redux";
 
-import { getLists } from '../store/actions/listActions';
-import globalStyles from './../styles/global';
-import Lists from '../components/Lists';
-import CustomButton from '../components/CustomButton';
+import { getLists } from "../store/actions/listActions";
+import globalStyles from "./../styles/global";
+import Lists from "../components/Lists";
+import CustomButton from "../components/CustomButton";
 
 const HomeScreen = ({ navigation }) => {
-    const [loading, setLoading] = useState(true);
-    const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getLists(() => setLoading(false)));
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getLists(() => setLoading(false)));
+  }, [dispatch]);
 
-    if (loading) {
-        return <ActivityIndicator color={Colors[1]} size="large" style={globalStyles.loader} />;
-    }
-
-    return(
-        <View style={styles.container}>
-            <Lists />
-            <CustomButton text="Agregar nueva lista" icon="add" iconColor="#fff" onPress={() => navigation.navigate('NewList')} />
-        </View>
+  if (loading) {
+    return (
+      <ActivityIndicator
+        color={Colors[1]}
+        size="large"
+        style={globalStyles.loader}
+      />
     );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Lists navigation={navigation} />
+      <CustomButton
+        text="Agregar nueva lista"
+        icon="add"
+        iconColor="#fff"
+        onPress={() => navigation.navigate("NewList")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-
-    },
+  container: {
+    flex: 1,
+  },
 });
 
 export default HomeScreen;
-
